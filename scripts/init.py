10aa7399bd08a4661487d150540d47bd48e7edb9
Chang Jie Leong <48868362+kylejeanlewis@users.noreply.github.com>
2024-11-28 10:52:52 +0800

    Update init.py

--- scripts/init.py
+++ scripts/init.py
@@ -1,19 +1,1 @@
-from pathlib import Path
-import sys
-REPO = 'my-repo'
-ROOT = str(Path().absolute()).split(REPO)[0]
-sys.path.append(f'{ROOT}{REPO}')
 
-from controllably import Helper
-Helper.get_ports()
-
-registry = Helper.read_yaml(f'{ROOT}{REPO}\\tools\\registry.yaml')
-"""Registry of device addresses"""
-addresses = registry['machine_id'][Helper.get_node()]
-"""Device addresses for this machine"""
-
-# Import plugins
-from library.plugins.my_plugin import MyClass
-
-# Define utility functions
-...
